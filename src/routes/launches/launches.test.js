c0d1071d3a04ea74016891b70575f9fa66439a9e
S-O <s@example.com>
2023-11-05 12:04:34 +0530

    load planet data before test run on github action

--- src/routes/launches/launches.test.js
+++ src/routes/launches/launches.test.js
@@ -1,16 +1,18 @@
 const request = require("supertest");
 
 const app = require("../../app");
+const { loadPlanetsData } = require("../../models/planets.model");
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 
 describe("Launches API", () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
 
-  afterAll(async()=>{
-    await mongoDisconnect()
-  })
+  afterAll(async () => {
+    await mongoDisconnect();
+  });
 
   /** GET /launches */
   describe("Test GET /v1/launches", () => {
